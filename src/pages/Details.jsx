08d06ef3b048ef70bcfdd4d3e403cfd506c41f77
Daniel Antonio <daniel23.da74@gmail.com>
2022-02-09 18:36:10 -0600

    Agregando detalles de la pelicula

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -6,31 +6,41 @@ import { moviesContext } from './../context/MoviesContext';
 
 export default function Details() {
   const { id } = useParams();
-  const { movies, reviews, setReviews } = useContext(moviesContext);
-  const comentario = useRef();
 
-  const movie = movies.filter((movie) => movie.id === id);
+  const { movies, reviews, setReviews } = useContext(moviesContext);
+
+  const comentario = useRef();
+  const rating = useRef();
+
+  const movie = movies.filter((movie) => movie.id === id)[0];
 
   // Por si la ruta no existe
-  if (movie.length === 0) {
+  if (!movie) {
     return <Navigate to="/NotFound" />;
   }
 
   // Función para agregar un comentario
   const addReview = () => {
     let valueComment = comentario.current.value;
-    console.log(valueComment);
-    setReviews([...reviews, { id: movie.id, valueComment }]);
+    setReviews([
+      ...reviews,
+      { id: reviews.length, idMovie: movie.id, comment: valueComment },
+    ]);
   };
 
   return (
-    <div>
-      {/* <p>Details {id}</p> */}
-      <Movies movies={movie} />
+    <>
+      <Movies movies={[movie]}></Movies>
       <div>
         <input ref={comentario} type="text" placeholder="comentario" />
+        <select ref={rating}></select>
         <button onClick={addReview}>Agregar comentario</button>
       </div>
-    </div>
+      {reviews.map((review, index) => {
+        return review.idMovie === id ? (
+          <p key={index}>{review.comment}</p>
+        ) : undefined;
+      })}
+    </>
   );
 }
